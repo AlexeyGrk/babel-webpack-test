05985cd577ddb0c87ec6be52a9861144e8e5dc77
AlexeyGrk <alexgmirko@gmail.com>
2021-04-20 13:54:01 +0300

    добавляет стартовую сборку вебпака вместе с бабелем

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello js");
+import "./styles.scss";
+import sumNumbers from "./utils";
+
+const num1Input = document.getElementById("num1");
+const num2Input = document.getElementById("num2");
+const bnt = document.getElementById("sumBtn");
+const result = document.getElementById("result");
+
+bnt.addEventListener("click", (e) => {
+  const num1 = Number(num1Input.value);
+  const num2 = Number(num2Input.value);
+  result.innerHTML = sumNumbers(num1, num2);
+});
